5499a30f91f8120e25ea376dcf7f5ad2dfacfd1c
krg7880 <kirkgordon7880@gmail.com>
2015-05-23 17:28:20 -0400

    Initial commit

---
+++ components/printFirst.js
@@ -0,0 +1,18 @@
+var noflo = require('noflo');
+
+exports.getComponent = function () {
+  var c = new noflo.Component();
+
+  c.inPorts.add('in', function (event, payload) {
+    if (event !== 'data') {
+      return;
+    }
+    // Do something with the packet, then
+    
+    console.log(payload[0]);
+    
+    c.outPorts.out.send(payload);
+  });
+  c.outPorts.add('out');
+  return c;
+};
